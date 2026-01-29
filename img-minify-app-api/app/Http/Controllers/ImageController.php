6b5fcb828a9a623e7e2448dbca761b2b7442bf96
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-01-29 12:08:57 +0800

    Open API swagger files

--- img-minify-app-api/app/Http/Controllers/ImageController.php
+++ img-minify-app-api/app/Http/Controllers/ImageController.php
@@ -6,6 +6,25 @@ use Illuminate\Http\JsonResponse;
 
 class ImageController extends Controller
 {
+    /**
+     * @OA\Post(
+     *     path="/api/image",
+     *     operationId="",
+     *     tags={""},
+     *     summary="",
+     *     description="",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="",
+     *         @OA\JsonContent(ref="#/components/schemas/User")
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="",
+     *         @OA\JsonContent(ref="#/components/schemas/User")
+     *     ),
+     * )
+     */
     public function optimizeImage(Request $request): JsonResponse
     {
         return response()->json([
@@ -15,6 +34,25 @@ class ImageController extends Controller
         ], 200);
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/image",
+     *     operationId="",
+     *     tags={""},
+     *     summary="",
+     *     description="",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="",
+     *         @OA\JsonContent(ref="#/components/schemas/User")
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="",
+     *         @OA\JsonContent(ref="#/components/schemas/User")
+     *     ),
+     * )
+     */
     public function getOptimizedImage(Request $request): JsonResponse
     {
         return response()->json([
