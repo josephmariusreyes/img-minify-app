f02b7536089a2f753362d359085c35412bd406fb
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-01-31 16:39:11 +0800

    Fix import issue in namespace

--- img-minify-app-api/routes/api.php
+++ img-minify-app-api/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers\ImageController;
+use App\Http\Controllers\ImageController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
